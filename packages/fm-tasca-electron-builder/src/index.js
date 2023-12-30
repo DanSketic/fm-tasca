943e7b3e2e2281dba8efb547fa605366e553f29f
DanSketic <dansketic@gmail.com>
2023-12-30 09:43:15 +0100

    Electron-Builder update

--- packages/fm-tasca-electron-builder/src/index.js
+++ packages/fm-tasca-electron-builder/src/index.js
@@ -3,33 +3,33 @@ const dargs = require('dargs')
 const path = require('path')
 const chalk = require('chalk')
 
-function getElectronBuilderBin(rootDir){
-	if(process.platform === 'win32'){
+function getElectronBuilderBin(rootDir) {
+	if (process.platform === 'win32') {
 		return path.join(rootDir, 'node_modules', '.bin', 'electron-builder.cmd')
-	}else {
+	} else {
 		return path.join(rootDir, 'node_modules', '.bin', 'electron-builder')
 	}
 }
 
-function useElectronBuilder(rootDir,projectDir, args = {}){
-	
+function useElectronBuilder(rootDir, projectDir, args = {}) {
+
 	const electronBuilderBin = getElectronBuilderBin(rootDir)
-	
+
 	return {
-		connect(task){
+		connect(task) {
 			task.watch()
 			task.pass()
-			
+
 			const electronBuilderProcess = spawn(electronBuilderBin, ['build', ...dargs({
 				project: projectDir,
 				...args
-			}) ],{
+			})], {
 				stdio: null
 			})
 
 			electronBuilderProcess.stdout.on('data', (data) => {
-				const purifiedData = data.toString().replace(/^\s*$/,'')
-				if(purifiedData !== '' && !data.includes('•')){
+				const purifiedData = data.toString().replace(/^\s*$/, '')
+				if (purifiedData !== '' && !data.includes('•')) {
 					task.print(chalk.yellow(` →✨ [electron-builder]:  • ${data}`))
 				}
 			})
